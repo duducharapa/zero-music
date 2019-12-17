b18d530d84c5f5d8782a3c9476e123f57605665c
Eduardo Charapa <eduardocharapa@gmail.com>
2019-12-17 14:29:53 -0200

    Adição do React-router-dom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import {
+     BrowserRouter,
+     Switch,
+     Route
+} from 'react-router-dom';
 
 import './index.css';
 import './scss/index.scss';
-import Home from './views/home';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+import Home from './views/home';
+import Musics from './views/musics';
+
+ReactDOM.render(
+     <BrowserRouter>
+          <Switch>
+               <Route path="/musics">
+                    <Musics />
+               </Route>
+               <Route path="/">
+                    <Home />
+               </Route>
+          </Switch>
+     </BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/views/musics.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+
+export default class Musics extends Component {
+     render() {
+          return (
+               <div>
+                    <Navbar />
+                    <Footer />
+               </div>
+          )
+     }
+}
