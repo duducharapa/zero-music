70b8166ded8ec1515d432d432d04d97d7dd21469
Eduardo Charapa <eduardocharapa@gmail.com>
2020-01-08 02:07:33 -0200

    Substituição da estilização SASS para styled-components no componente Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,41 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { routes } from '../../helpers/routes';
+import styled from 'styled-components';
+
+// Styled Components
+const StyledFooter = styled.footer`
+     padding: 50px 0px 5px 0px;
+     background: linear-gradient(#AB87FF,#52489C);
+
+     ul {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          margin: 5px 0;
+
+          li {
+               margin: 0 5px;
+          }
+     }
+
+     button {
+          background: none;
+          border: none;
+          color: #EFF8E2;
+          font-size: 1rem;
+
+          &:hover {
+               cursor: pointer;
+          }
+     }
+
+     p {
+          text-align: center;
+          color: #C6962D;
+     }
+`;
 
 export function Footer(){
 
@@ -15,29 +50,23 @@ export function Footer(){
      }
 
      return (
-          <footer className="footer">
-               <nav>
-                    <ul className="footer-menu">
-                         {
-                              routes.map( ({ path, text }) => (
-                                   <li key={ text } className="footer-menu-item">
-                                        <button 
-                                             className="footer-menu-link"
-                                             onClick={ () => handleClick(path) }
-                                        >
-                                             { text }
-                                        </button>
-                                   </li>
-                              ))
-                         }
-                    </ul>
-               </nav>
-               <p className="footer-credits">
-                    Copyright - Eduardo Charapa
-               </p>
+          <StyledFooter>
+               <ul>
+                    {
+                         routes.map( ({ path, text }) => (
+                              <li key={ text }>
+                                   <button onClick={ () => handleClick(path) }>
+                                        { text }
+                                   </button>
+                              </li>
+                         ))
+                    }
+               </ul>
+
+               <p> Copyright - Eduardo Charapa </p>
 
                { clicked ? <Redirect to={ path } /> : null }
-          </footer>
+          </StyledFooter>
      );
 
 }
